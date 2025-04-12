a22af0fea41b8b6eb81ed2e915504f9bd25db575
felicitasreilly <freilly@udesa.edu.ar>
2025-04-11 23:07:10 -0300

    Mi perfil COMPLETO y comentarios

--- express-node-ml/controllers/userController.js
+++ express-node-ml/controllers/userController.js
@@ -1,15 +1,18 @@
-
+let datosProductos = require('../db/index')
 const userController = {
     inicioSesion: function(req, res){
         res.render('login');
     },
 
     perfil: function(req, res){
-        res.render('profile');
+         res.render('profile', {
+            usuario: datosProductos.usuario,
+            nombreUsuario: datosProductos.usuario.usuario
+        });
     },
 
     registro: function(req, res){
-        res.render('register');
+        res.render('register', {nombreUsuario: datosProductos.usuario.usuario});
     },
 }
 
--- express-node-ml/db/index.js
+++ express-node-ml/db/index.js
@@ -42,21 +42,19 @@ const datosProductos = {
             descripcion: 'Laptop modelo macbook pro del año 2019',
             comentarios: [
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'mariana23',
+                    comentario: 'Excelente servicio, súper recomendable.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'juan_tech',
+                    comentario: 'El producto llegó rápido y en buen estado.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'lucasdev',
+                    comentario: 'No era lo que esperaba, pero igual cumplió.',
+                    imagen: 'default-image.png',
                 },
             ]
         },
@@ -68,15 +66,19 @@ const datosProductos = {
             descripcion: 'Laptop modelo macbook pro del año 2019',
             comentarios: [
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'sofi_gamer',
+                    comentario: 'Increíble calidad, superó mis expectativas.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'maxi_art',
+                    comentario: 'Muy buen diseño, volvería a comprar.',
+                    imagen: 'default-image.png',
+                },
+                {
+                    nombre: 'ana.codes',
+                    comentario: 'El envío tardó un poco, pero valió la pena.',
+                    imagen: 'default-image.png',
                 },
             ]
         },
@@ -88,21 +90,19 @@ const datosProductos = {
             descripcion: 'Laptop modelo macbook pro del año 2019',
             comentarios: [
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'auto_fan23',
+                    comentario: 'El motor suena increíble, súper recomendable.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'speedy_lucas',
+                    comentario: 'Ideal para los que aman la velocidad. 10/10.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'claudia_ruta',
+                    comentario: 'Cómodo, seguro y elegante. Me encantó.',
+                    imagen: 'default-image.png',
                 },
             ]
         },
@@ -114,21 +114,19 @@ const datosProductos = {
             descripcion: '',
             comentarios: [
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'mecanico_juan',
+                    comentario: 'Excelente suspensión y muy buen consumo.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'rocioviajera',
+                    comentario: 'Lo usamos para un viaje largo y respondió perfecto.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'ale_turbo',
+                    comentario: 'Muy buena relación precio-calidad.',
+                    imagen: 'default-image.png',
                 },
             ]
         },
@@ -140,21 +138,19 @@ const datosProductos = {
             descripcion: '',
             comentarios: [
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'carlos_racing',
+                    comentario: '¡El coche es una bestia! Increíble aceleración.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'luisa_drive',
+                    comentario: 'Súper cómodo para viajes largos, lo recomiendo.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'max_rider',
+                    comentario: 'Me encantó el diseño, pero el consumo podría mejorar.',
+                    imagen: 'default-image.png',
                 },
             ]
         },
@@ -166,21 +162,19 @@ const datosProductos = {
             descripcion: '',
             comentarios: [
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'juanito_motor',
+                    comentario: 'Muy buen rendimiento, ideal para ciudad y carretera.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'maria_turismo',
+                    comentario: 'Excelente para viajes largos, súper cómodo y seguro.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'fer_autoentusiasta',
+                    comentario: 'La conducción es suave, pero el consumo de combustible es algo elevado.',
+                    imagen: 'default-image.png',
                 },
             ]
         },
@@ -192,21 +186,19 @@ const datosProductos = {
             descripcion: '',
             comentarios: [
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'ricardo_rally',
+                    comentario: 'Ideal para los amantes de la velocidad, el mejor de su categoría.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'pablo_drive',
+                    comentario: 'Excelente calidad de materiales y confort en el interior.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'carla_circuito',
+                    comentario: 'Buena performance, aunque podría mejorar la tecnología a bordo.',
+                    imagen: 'default-image.png',
                 },
             ]
         },
@@ -218,21 +210,19 @@ const datosProductos = {
             descripcion: '',
             comentarios: [
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'jose_roadtrip',
+                    comentario: 'Gran coche para viajes largos, súper estable y cómodo.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'sofia_motor',
+                    comentario: 'Rendimiento increíble en la pista, muy ágil y rápido.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'martin_mec',
+                    comentario: 'Excelente tecnología de seguridad, pero algo ruidoso en carretera.',
+                    imagen: 'default-image.png',
                 },
             ]
         },
@@ -244,21 +234,19 @@ const datosProductos = {
             descripcion: '',
             comentarios: [
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'luis_carrera',
+                    comentario: 'Este coche es una máquina, ¡perfecto para los fanáticos de la velocidad!',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'victoria_viajera',
+                    comentario: 'Comodidad y eficiencia en el consumo de combustible, ideal para largos recorridos.',
+                    imagen: 'default-image.png',
                 },
-
                 {
-                    nombre: '',
-                    comentario: '',
-                    imagen: '',
+                    nombre: 'pedro_turbo',
+                    comentario: 'Me gusta el diseño, pero podría mejorar la suspensión en caminos irregulares.',
+                    imagen: 'default-image.png',
                 },
             ]
         }
