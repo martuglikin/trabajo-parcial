f62cd7f7fb18961d325a54020dc8e2666e0cee64
milagros alonso <malonsoboto@udesa.edu.ar>
2025-05-25 12:29:27 -0300

    modelo Producto

---
+++ express-node-ml/database/models/Producto.js
@@ -0,0 +1,45 @@
+module.exports = function (sequelize, dataTypes ) {
+    let alias = "Producto";
+    let cols = {
+        id: {
+            autoIncrement : true,
+            primaryKey : true,
+            notNull: true,
+            type : dataTypes.INTEGER.UNSIGNED
+        },
+        usuario_id: {
+            type : dataTypes.INTEGER.UNSIGNED,
+            notNull: true, 
+        },
+        imagen_producto: {
+            type : dataTypes.STRING,
+            notNull: true, 
+        },
+        nombre_producto: {
+            type : dataTypes.STRING,
+            notNull: true, 
+        },
+        descripcion: {
+            type : dataTypes.STRING,
+            notNull: true, 
+        },
+        createdAt: {
+            type : dataTypes.DATE,
+        },
+        updatedAt: {
+            type : dataTypes.DATE,
+        },
+        deletedAt: {
+            type : dataTypes.DATE,
+        }
+  	}
+
+    let config = {
+        tableName: "productos",
+        timestamps: true,
+        underscored: false
+    }
+    
+    let Producto = sequelize.define(alias, cols, config);
+    return Producto;
+}
