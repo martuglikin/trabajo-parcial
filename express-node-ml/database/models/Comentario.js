08bc522ee1dea1db124e9c16b595e07ebc387642
felicitasreilly <freilly@udesa.edu.ar>
2025-05-26 01:38:32 +1000

    ModeloComentario

---
+++ express-node-ml/database/models/Comentario.js
@@ -0,0 +1,57 @@
+module.exports = function (sequelize, dataTypes) {
+    let alias = "Comentario";
+    let cols = {
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            notNull: true,
+            type: dataTypes.INTEGER.UNSIGNED
+        },
+        producto_id: {
+            type: dataTypes.INTEGER.UNSIGNED,
+            notNull: true,
+        },
+        usuario_id: {
+            type: dataTypes.INTEGER.UNSIGNED,
+            notNull: true,
+        },
+        texto: {
+            type: dataTypes.STRING,
+            notNull: true,
+        },
+        createdAt: {
+            type: dataTypes.DATE,
+        },
+        updatedAt: {
+            type: dataTypes.DATE,
+        },
+        deletedAt: {
+            type: dataTypes.DATE,
+        }
+    }
+
+    let config = {
+        tableName: "comentarios",
+        timestamps: true,
+        underscored: false
+    }
+
+    let Comentario = sequelize.define(alias, cols, config);
+
+
+    Comentario.associate = function (models) {
+        Comentario.belongsTo(models.User, {
+
+            as: "comentador",
+
+            foreignKey: "usuario_id"
+        });
+        Comentario.belongsTo(models.Producto, {
+
+            as: "producto",
+
+            foreignKey: "producto_id"
+        });
+    }
+    return Comentario;
+}
